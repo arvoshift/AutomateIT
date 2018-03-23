a0feaadc983a4f791ca350e2acb554e3e4075448
Ben Philippi <ben.philippi@overthewire.com.au>
2018-03-23 11:14:54 +1000

    Just SD only now.

--- scraping.py
+++ scraping.py
@@ -2,31 +2,27 @@ from lxml import html
 import requests
 
 # create page object which gets() the github pricing page
-page = requests.get('https://github.com/pricing')
+#page = requests.get('https://github.com/pricing')
 
 # interpret the string page.content as html and assign it to the tree object.
-tree = html.fromstring(page.content)
+#tree = html.fromstring(page.content)
 
-print("Page object:", tree)
+#print("Page object:", tree)
 
 # what does xpath do?
-plans = tree.xpath('//h2[@class="alt-h3"]/text()')
-pricing = tree.xpath('//span[@class="local-currency"]/text()')
+#plans = tree.xpath('//h2[@class="alt-h3"]/text()')
+#pricing = tree.xpath('//span[@class="local-currency"]/text()')
 
-print("Plans:", plans, "\nPricing:", pricing)
-
-
-# TODO(benp): scrape from other sites.
+#print("Plans:", plans, "\nPricing:", pricing)
 
 page1 = requests.get('https://www.slashdot.org/')
 
 tree1 = html.fromstring(page1.content)
 
-print("Page object:", tree1)
+#print("Page object:", tree1)
 
-posts = tree1.xpath('//h2[@class="story"]/text')
-print("Posts:", posts, "\n")
-
-
+posts = tree1.xpath('//span[@class="story-title"]/a/text()')
+for post in posts:
+	print("Post:", post)
 
 # EOF
