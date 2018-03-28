6f71056c87439c4428ce9f6a30cfcf5ab99fb9f1
Ben Philippi <ben.philippi@overthewire.com.au>
2018-03-29 08:36:14 +1000

    - using the standard urllib.request library instead.  - download the images.

--- scraping2.py
+++ scraping2.py
@@ -17,3 +17,14 @@ header = {'User-Agent': 'Mozilla/5.0'}
 #soup = BeautifulSoup(urllib.urlopen(urllib.Request(url, headers=header)))
 soup = BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url, headers=header)), "lxml")
 
+images = [a['src'] for a in soup.find_all("img", {"src": re.compile("gstatic.com")})][:5]
+for img in images:
+	print("image source: ", images)
+
+for img in images:
+	raw_img = urllib.request.urlopen(img).read()
+	cntr = len([i for i in os.listdir(".") if image_type in i]) + 1
+	f = open(image_type + "_"+ str(cntr)+".jpg", 'wb')
+	f.write(raw_img)
+	f.close()
+
